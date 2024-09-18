6a3f82b12c3cfd3f7848ab97fb0cfe5842239631
Katy Cespedes <angel@gmail.com>
2024-09-18 17:29:12 -0500

    update greet function to include a friendly message

--- scrip.py
+++ scrip.py
@@ -1,7 +1,7 @@
 # script.py
 
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello, {name}! How are you today?"
 
 if __name__ == "__main__":
-    print(greet("World"))
+    print(greet("world"))
